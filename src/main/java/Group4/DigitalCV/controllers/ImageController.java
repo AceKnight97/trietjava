d8a9a66ab7609ee511fbada7968434d588eb4cfb
APAC\TTO7HC <Triet.TruongThanh@vn.Bosch.com>
2021-12-22 17:36:50 +0700

    delete image model

--- src/main/java/Group4/DigitalCV/controllers/ImageController.java
+++
@@ -1,5 +0,0 @@
-package Group4.DigitalCV.controllers;
-
-public class ImageController {
-
-}
--- src/main/java/Group4/DigitalCV/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package Group4.DigitalCV.repository;
-
-import java.util.Optional;
-import Group4.DigitalCV.model.ImageModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<ImageModel, Long> {
-    Optional<ImageModel> findByName(String name);
-}
